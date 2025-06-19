c09398f6b1188c5c063ff7a4f8b81af344d9c9b0
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-19 14:15:25 +0900

    Array -> ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Box {
-    public static <T> void displayArray(List<T> array) {
-        for(T element: array) {
-            System.out.println(element);
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
-
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-
-        List<String> words = new ArrayList<>();
-        words.add("apple");
-        words.add("banana");
-        words.add("cherry");
-
-
-        Box.displayArray(numbers);
-        Box.displayArray(words);
+        Object[] arr = new Integer[3];
     }
 }
