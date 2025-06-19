928d852514927e2cae35bd758dac770b49a3ee07
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-19 14:59:41 +0900

    printElements

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void printElements(List<?> list) {
-        for (Object o : list) {
-            System.out.println(o);
+
+    public static double sumList(List<? extends Number> list) {
+        double sum = 0;
+
+        for(Number n: list) {
+            sum += n.doubleValue();
         }
+
+        return sum;
     }
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3);
-        List<String> words = Arrays.asList("A", "B", "C");
+        List<Integer> intList = Arrays.asList(10, 20, 30);
+        List<Double> doubleList = Arrays.asList(0.1, 0.2);
 
-        printElements(numbers);
-        printElements(words);
+        System.out.println("합계: " + sumList(doubleList));
     }
 }
