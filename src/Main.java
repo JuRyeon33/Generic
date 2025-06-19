99e0f48869871222e76f14bf1013a3b4ce77300e
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-19 10:44:48 +0900

    Generics

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+class Box<T> {
+    private T content;
+}
+
+class StringBox {
+    private String content;
+}
+
+class IntegerBox {
+    private Integer content;
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
