24a568605939a919e28f8abb0d805f8090d6b964
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-19 10:49:16 +0900

    Generics test

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 class Box<T> {
     private T content;
+
+    public Box(T content) {
+        this.content = content;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public <E> void printContent(E extraInfo) {
+        System.out.println("Content : " + content + ", Extra : " + extraInfo);
+    }
 }
 
-class StringBox {
-    private String content;
-}
 
-class IntegerBox {
-    private Integer content;
-}
 
 public class Main {
     public static void main(String[] args) {
