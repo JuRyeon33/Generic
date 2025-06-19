c508a0864a731ad434e7bb434a0d1adf6f1d7a15
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-19 13:19:39 +0900

    Generics Box displayArray

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Box {
-    public static <T> void displayArray(T[]  array) {
+    public static <T> void displayArray(List<T> array) {
         for(T element: array) {
             System.out.println(element);
         }
@@ -8,8 +11,19 @@ class Box {
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] numbers = {1, 2, 3, 4, 5};
-        String[] words = {"apple", "banana", "cherry"};
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("cherry");
+
 
         Box.displayArray(numbers);
         Box.displayArray(words);
